1e44bcd34965b3a2ace441ff7369e95fde2bc873
Nina <janina.byszewska@gmail.com>
2025-02-05 12:11:20 +0100

    feature: trigger search 2s after user stops typing

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,8 +1,9 @@
+import { useCallback, useEffect } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { useSearchParams } from 'react-router-dom'
 import * as yup from 'yup'
-import { Box, TextField } from '@mui/material'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { Box, TextField } from '@mui/material'
 
 export const USERNAME_QUERY_KEY = 'username'
 
@@ -23,6 +24,7 @@ export const SearchBox = () => {
     register,
     handleSubmit,
     formState: { errors },
+    watch,
   } = useForm({
     resolver: yupResolver(searchSchema),
     defaultValues: {
@@ -30,13 +32,26 @@ export const SearchBox = () => {
     },
   })
 
-  const onSearchSubmit: SubmitHandler<SearchFormValues> = ({ username }) => {
-    const encodedUsername = encodeURIComponent(username ?? '')
-    setSearchParams({ username: encodedUsername })
-  }
+  const onSubmit: SubmitHandler<SearchFormValues> = useCallback(
+    ({ username }) => {
+      setSearchParams({ username: encodeURIComponent(username ?? '') })
+    },
+    [setSearchParams]
+  )
+
+  const usernameWatch = watch('username')
+
+  // triggger search 2s after user stops typing
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      handleSubmit(onSubmit)()
+    }, 2000)
+
+    return () => clearTimeout(timer)
+  }, [usernameWatch, handleSubmit, onSubmit])
 
   return (
-    <Box component="form" onSubmit={handleSubmit(onSearchSubmit)}>
+    <Box component="form" onSubmit={handleSubmit(onSubmit)}>
       <TextField
         autoFocus
         fullWidth
